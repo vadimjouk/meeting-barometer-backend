8f81580854a992634dc63c30f69ce61fcb5cbebf
Vadim Jouk <vadim.jouk@gmail.com>
2022-10-03 19:41:21 +0200

    xxx

--- src/main/java/de/vj/mb/backend/routes/ApiV1RestRoutes.java
+++ src/main/java/de/vj/mb/backend/routes/ApiV1RestRoutes.java
@@ -12,25 +12,20 @@ public class ApiV1RestRoutes extends RouteBuilder {
 
 	@Override
 	public void configure() throws Exception {
-		this.restConfiguration()
-				.component("servlet").bindingMode(RestBindingMode.json)//
+		this.restConfiguration().component("servlet").bindingMode(RestBindingMode.json)//
 				.dataFormatProperty("prettyPrint", "true")//
 				.contextPath("/").port(8080)//
 				.apiContextPath("/api-doc")//
 				.apiProperty("api.title", "Meeting Barometer API").apiProperty("api.version", "1")//
-				.apiProperty("swagger.version", "2")
-				.apiProperty("cors", "true")
-				.enableCORS(true)
-				.corsAllowCredentials(true)
-				.corsHeaderProperty("Access-Control-Allow-Headers",
+				.apiProperty("swagger.version", "2").apiProperty("cors", "true").enableCORS(true).corsAllowCredentials(true).corsHeaderProperty("Access-Control-Allow-Headers",
 						"Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization, Authorization-User");
 
-		this.rest("/api/v1").description("Meeting Barometer Backend")
-				.consumes(MediaType.APPLICATION_JSON_VALUE)
-				.produces(MediaType.APPLICATION_JSON_VALUE);
+		this.rest("/api/v1").description("Meeting Barometer Backend").consumes(MediaType.APPLICATION_JSON_VALUE).produces(MediaType.APPLICATION_JSON_VALUE);
 
-		this.rest("/api/v1/feedbacks/{meetingId}")
-				.get("/").description("List all Feedbacks for Meeting").outType(Feedback[].class) //
+		this.rest("/api/v1/feedbacks/{meetingId}").get("/").description("List all Feedbacks for Meeting").outType(Feedback[].class) //
+				.to("direct:listFeedbacksByMeetingId");
+
+		this.rest("/api/v1/reports/{meetingId}").get("/").description("List all Feedbacks for Meeting").outType(Feedback[].class) //
 				.to("direct:listFeedbacksByMeetingId");
 
 		this.from("direct:listFeedbacksByMeetingId").to("bean:feedbackService?method=getFeedbackList(${headers.meetingId})");
